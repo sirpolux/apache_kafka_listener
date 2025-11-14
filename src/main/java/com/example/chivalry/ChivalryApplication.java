c605007330a516dfb2ff58ebe03e88a1217d2865
Machong Paul <59781024+sirpolux@users.noreply.github.com>
2025-11-14 10:59:53 +0100

    first push

---
+++ src/main/java/com/example/chivalry/ChivalryApplication.java
@@ -0,0 +1,13 @@
+package com.example.chivalry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChivalryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChivalryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chivalry/ChivalryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chivalry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChivalryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
